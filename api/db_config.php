bf2aaf40d043e3861481699854c6cec800e2c9a0
Aziz Boudani <boudani@gmail.com>
2020-09-09 16:30:20 +0200

    first commit

---
+++ api/db_config.php
@@ -0,0 +1,10 @@
+<?php
+  
+    $hostName = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "your_DB";
+ 
+    $mysqli = new mysqli($hostName, $username, $password, $dbname);
+ 
+?>
